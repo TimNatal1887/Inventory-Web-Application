fa208a505f689526bab502e79f16a439e54e151d
Timothy Natal <timothynatal@pursuit.org>
2023-11-05 08:15:57 -0500

    Function to add game to list partially working

--- scripts.js
+++ scripts.js
@@ -1,35 +1,41 @@
 const form = document.querySelector("form");
+const games = document.querySelector(".game-list")
+console.log(games)
 
 form.addEventListener("submit",(event) =>{
     event.preventDefault();
-    const {title,dev,image,price,description} = event.target
-    createGameTemplate(title.value,dev.value,image.value,price.value,description.value,stock.value)
+
+    // if(elementCheck(info)){
+    //     elementCheck(info)
+    // }else{
+        const {title,dev,image,price,description,stock} = event.target
+        createGameTemplate(title.value,dev.value,image.value,price.value,description.value,stock.value)
+    // }
 })
 
-function createGameTemplate(title,dev,image,price,description,stock){
-    const info = [title,dev,image,price,description]
-    const stockText = stockCheck(stock)
-    if(elementCheck(info)){
-        return elementCheck(info)
-    }
 
+function createGameTemplate(title,dev,image,price,description,stock){
+    const newGame = document.createElement("li")
+    newGame.classList.add("game")
+    newGame.innerHTML = `<div id="list-header">
+    <h3 id="title-output">${title}</h3>
+    <img src="${image}">
+    <div id="developer-output">Developed by: ${dev}</div>
+    <div id="price-output">Price:${price}</div>
+    <div id="description-output">${description}</div>
+    <div id="stock-value">
+    <button class="${stock}"></button>
+    <button id="remove">Remove</button>
+    </div>`
+    games.append(newGame)
+    
     const button = newGame.querySelector("#remove")
     button.addEventListener("click",() =>{
         newGame.remove()
     })
 
-    
-    const newGame = document.createElement("li")
-    newGame.innerHTML = `<div id="list-header">
-    <h3 id="title-output">${title}</h3>
-    <img src="${image}"
-    <div id="developer-output">Developed by: ${dev}</div>
-    <div id="price-output">Price:${price}</div>
-    <div id="description-output">${description}</div>
-    <div id="stock-value">
-    <button class="${stock}">${stockText}</button>
-    <button id="remove">Remove</button>
-    </div>`
+
+
     
 }
 
@@ -47,3 +53,4 @@ function elementCheck(elements){
     }
     return false
 }
+
